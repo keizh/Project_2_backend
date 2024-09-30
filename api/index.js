a27ab03daa7bfb49511d6e421f7de0b5886c294d
Keizh <krishnamulay05@gmail.com>
2024-09-30 19:25:31 +0530

    mongoDB connection istablsihed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/user", (req, res) => res.send("Express on User"));
 
-app.listen(5500, () => console.log("Server ready on port 3000."));
+async function exe() {
+  try {
+    const MONGODBconnectionOBJ = await mongoose.connect(process.env.MONGODB);
+    if (MONGODBconnectionOBJ) {
+      app.listen(5500, () =>
+        console.log("SERVER IS ONLINE WITH MONGODB Connection established")
+      );
+    } else {
+      console.log(`Failed to connect to MongoDB and server not created`);
+    }
+  } catch (err) {
+    console.log(`${err.message}`);
+  }
+}
+exe();
 
 module.exports = app;
