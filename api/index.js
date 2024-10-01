b2a0bfc29c83f7b3871b1f908e5e73b2c5b6602c
Keizh <krishnamulay05@gmail.com>
2024-10-01 13:08:53 +0530

    meaning less connect route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const exe = require("../db/exe");
+var connected = false;
+// const exe = require("../db/exe");
+async function exe() {
+  try {
+    const MONGODBconnectionOBJ = await mongoose.connect(process.env.MONGODB);
+    if (MONGODBconnectionOBJ) {
+      console.log("MONGODB Connection established");
+      connected = true;
+    } else {
+      console.log(`Failed to connect to MongoDB and server not created`);
+    }
+  } catch (err) {
+    console.log(`${err.message}`);
+  }
+}
 exe();
 const corsOptions = {
   origin: ["*"],
@@ -17,11 +31,11 @@ app.use(cors(corsOptions));
 // middleware to parse body
 app.use(express.json());
 // most basic api-endpoint
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send(`Express on Vercel`));
+app.get("/connect", (req, res) => res.send(`Is it connected: ${connected}`));
 const userRoutes = require(`../Routes/userRoutes`);
 app.use("/api/v1/user", userRoutes);
 
-// code to setUp MONGOdb CONNECTION AND MAKE SERVER LISTEN
-
-app.listen(5500, () => console.log("SERVER IS ONLINE "));
+// MAKE SERVER LISTEN
+app.listen(process.env.PORT, () => console.log("SERVER IS ONLINE "));
 module.exports = app;
