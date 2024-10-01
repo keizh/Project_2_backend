6aaa4f055a817de473491c9803d5a639b22aa351
Keizh <krishnamulay05@gmail.com>
2024-10-01 15:22:03 +0530

    userModel is completed

--- models/user.model.js
+++ models/user.model.js
@@ -1,20 +1,85 @@
 const mongoose = require(`mongoose`);
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const followUserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
+  { timestamps: true }
+);
+
+const bookmarkSchema = new Schema(
+  {
+    postImage: {
+      type: String,
+      required: true,
+    },
+    postUserName: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    postContent: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  { timestamps: true }
+);
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default:
+        "New to Hashtag, exploring and connecting with the world! Let's stay connected and discover together.",
+    },
+    profileImage: {
+      type: String,
+      default: `https://placehold.co/600x400?text=New+User`,
+    },
+    following: [followUserSchema],
+    followers: [followUserSchema],
+    bookmarks: [bookmarkSchema],
+    createdOn: {
+      type: String,
+      default: `${new Date().getDate()} / ${
+        new Date().getMonth() + 1
+      } / ${new Date().getFullYear()} `,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const userModel = model("user", userSchema);
 
