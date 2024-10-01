4d8545a40dca1a5ba10e209b2a8b4d0474e3b9d3
Keizh <krishnamulay05@gmail.com>
2024-10-01 11:46:45 +0530

    chore:chaged the exe()

--- api/index.js
+++ api/index.js
@@ -24,9 +24,7 @@ async function exe() {
   try {
     const MONGODBconnectionOBJ = await mongoose.connect(process.env.MONGODB);
     if (MONGODBconnectionOBJ) {
-      app.listen(5500, () =>
-        console.log("SERVER IS ONLINE WITH MONGODB Connection established")
-      );
+      console.log("MONGODB Connection established");
     } else {
       console.log(`Failed to connect to MongoDB and server not created`);
     }
@@ -35,5 +33,5 @@ async function exe() {
   }
 }
 exe();
-
+app.listen(5500, () => console.log("SERVER IS ONLINE "));
 module.exports = app;
